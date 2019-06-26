3bd22da2459a9bdaf313fb64b1f3b118ed109b62
pnagaev <pnagaev@yandex.ru>
2019-06-26 08:48:50 +0300

    Update compare-objectz.ps1

--- compare-objectz.ps1
+++ compare-objectz.ps1
@@ -1,3 +1,15 @@
+<#
+Compare-Objectz.ps1
+
+С помощью этого скрипта можно сравнивать два объекта PowerShell одного типа и выводить на экран только различающиеся свойства.
+
+Это очень удобно при траблшутинге Exchange Server, когда у одного пользователя "что-нибудь" не работает, а у остальных работает и необходимо сравнить свойства почтового ящика.
+А также при миграциях, когда необходимо создать ещё один объект - Receive  connector, Virtual Directory и т.д.
+
+https://www.youtube.com/ITVideoPRO - канал про Exchange Server, PowerShell и безопасность.
+
+#>
+
 if ((Get-PSSession |where ConfigurationName -Eq "Microsoft.Exchange") -eq $null){
 
 $s = New-PSSession -ConfigurationName Microsoft.Exchange `
